60816c5168149081d99461480a4fa67886574802
Shreeji Tiwari <87295390+shreejitiwari@users.noreply.github.com>
2023-11-28 19:29:39 +0000

    Completed linked list implementation version 1

--- DSA/Linked List/Implementation Without creating LL class.cpp
+++ DSA/Linked List/Implementation Without creating LL class.cpp
@@ -22,6 +22,98 @@ void insertAtBeginning(Node * &head, int val)
     head = new_node;
 }
 
+void insertAtEnd(Node* head, int val)  // well not needed bcoz of insertAtPosition
+{
+    Node* new_node = new Node(val);
+    Node* temp  = head;
+
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = new_node;
+}
+
+void insertAtPosition(Node* head, int val, int position)
+{
+    if (position==0)
+        insertAtBeginning(head, val);
+
+    else
+    {
+        Node* temp = head;
+        Node* new_node = new Node(val);
+        int count=0;
+
+        while(temp->next != nullptr && count<position-1)
+        {
+            temp = temp->next;
+            count++;
+        }
+
+        new_node->next = temp->next;
+        temp->next = new_node;
+    }
+}
+
+void deleteFromBeginning(Node* &head)
+{
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+}
+
+void deleteAtEnd(Node* &head)
+{    
+    Node* cur = head;
+    if (cur->next == nullptr)
+    {
+        deleteFromBeginning(head);
+    }
+
+
+
+    while (cur->next->next != nullptr)
+    {
+        cur = cur->next;
+    }
+
+    Node* to_delete = cur->next;
+    cur->next = cur->next->next;
+    delete to_delete;        
+}
+
+void deleteAtPosition(Node* &head, int position)
+{
+    if (position==0) 
+    {
+        deleteFromBeginning(head);
+        return;
+    }
+
+    Node* cur = head;
+    int count = 0;
+    while (cur->next != nullptr && count!=position-1)
+    {
+        cur = cur->next;
+        count++;
+    }
+
+    
+    if (count+1 == position && cur->next != nullptr)
+    {        
+        Node* temp = cur->next;
+        cur->next = cur->next->next;
+        delete temp;     
+    }
+
+    else
+    {
+        cout<<"Linked List is shorter than "<<position<<endl;
+    }
+
+}
+
 void display(Node * head)
 {
     Node * temp = head;
@@ -37,12 +129,36 @@ void display(Node * head)
 
 int main()
 {
-    Node*  head = new Node(10);
+    Node*  head = nullptr;
+    display(head);
+    insertAtBeginning(head, 10);
     display(head);
     insertAtBeginning(head, 20);
     display(head);
     insertAtBeginning(head, 30);
     display(head);
+    insertAtEnd(head, 50);
+    display(head);
+    insertAtPosition(head, 15, 2);
+    display(head);
+    insertAtPosition(head, 25, 3);
+    display(head);
+    deleteFromBeginning(head);
+    display(head);
+    insertAtPosition(head, 456,5);
+    display(head);
+    deleteAtPosition(head, 2);
+    display(head);
+    deleteAtPosition(head, 40);
+    display(head);
+    
 
+    for (int i=4; i>=0; i--)
+    {
+        // deleteAtPosition(head, i);
+        // deleteAtEnd(head);
+        deleteFromBeginning(head);
+        display(head);
+    }
     return 0;
 }
